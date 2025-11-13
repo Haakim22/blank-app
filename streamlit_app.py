d708da2525aff6120fb08ee9db4e5916804ca743
Campanale Haakim Bin Yusuf <152841764+Haakim22@users.noreply.github.com>
2025-11-13 01:33:06 +0000

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,16 @@ router = clips.LoggingRouter()
 env.add_router(router)
 
 # input
-name = st.text_input
+name = st.text_input("Enter your name")
+
+# knowledge
+env.build("(deftemplate result (slot name))")
+env.assert_string(f'(result(name "{name}"))')
+env.run()
+
+results = []
+for fact in env.facts():
+    if fact.template.name == 'result' :
+        results.append(fact['name'])
+
+st.write(results[0],"output")
