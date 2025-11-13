674c08e7ab335cd9319063d5c02f98a5ceb6fc75
Campanale Haakim Bin Yusuf <152841764+Haakim22@users.noreply.github.com>
2025-11-13 01:47:21 +0000

    On branch main Your branch is up to date with 'origin/main/branch-1'.
     Changes to be committed:
    	modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import logging
 
 # Setup working environment
 logging.basicConfig(level=15, format='%(message)s')
-
+###
 env = clips.Environment()
 router = clips.LoggingRouter()
 env.add_router(router)
